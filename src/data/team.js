e31db42b6fd67b7b5c6574076b81f881455114d1
Aditya-v05 <adityaspark05@gmail.com>
2025-11-25 19:51:47 +0530

    changes to team section

--- src/data/team.js
+++ src/data/team.js
@@ -1,69 +1,69 @@
 // Team members data - Replace with actual team member information
 export const teamMembers = [
   {
-    name: "Team Member 1",
-    role: "Developer",
-    image: "/assets/photos/teamphotos/1000019953.jpg",
-    frameColor: "Blue"
-  },
-  {
-    name: "Team Member 2",
-    role: "Designer",
+    name: "Gopinath D",
+    role: "App Development Lead",
     image: "/assets/photos/teamphotos/1000055178-Picsart-AiImageEnhancer.jpg",
-    frameColor: "Red"
-  },
-  {
-    name: "Team Member 3",
-    role: "Marketing",
-    image: "/assets/photos/teamphotos/1000153223.jpg",
-    frameColor: "Green"
-  },
-  {
-    name: "Team Member 4",
-    role: "Content",
-    image: "/assets/photos/teamphotos/IMG_20250823_161602.jpg",
-    frameColor: "Yellow"
-  },
-  {
-    name: "Team Member 5",
-    role: "Developer",
-    image: "/assets/photos/teamphotos/IMG_20251103_164627.jpg",
     frameColor: "Blue"
   },
   {
-    name: "Team Member 6",
-    role: "Designer",
-    image: "/assets/photos/teamphotos/IMG-20251103-WA0031.jpg",
+    name: "Dharan R",
+    role: "Cyber Security Lead",
+    image: "/assets/photos/teamphotos/IMG-20251123-WA0015.jpg",
     frameColor: "Red"
   },
   {
-    name: "Team Member 7",
-    role: "Marketing",
+    name: "Gokula Bharathi L",
+    role: "Ai/ML Lead",
     image: "/assets/photos/teamphotos/IMG_20251123_123325.jpg",
     frameColor: "Green"
   },
   {
-    name: "Team Member 8",
-    role: "Content",
-    image: "/assets/photos/teamphotos/IMG-20251123-WA0015.jpg",
+    name: "Sakthivishwa M",
+    role: "Web Development Lead",
+    image: "/assets/photos/teamphotos/IMG_20241023_121843_757.jpg",
     frameColor: "Yellow"
   },
   {
-    name: "Team Member 9",
-    role: "Developer",
+    name: "Sethupathi K",
+    role: "Cloud & Devops Lead",
     image: "/assets/photos/teamphotos/Polish_20241017_221304347.png",
     frameColor: "Blue"
   },
   {
-    name: "Team Member 10",
-    role: "Designer",
+    name: "Maha Vidhya Sri L",
+    role: "Competitive Programming Lead",
     image: "/assets/photos/teamphotos/WhatsApp Image 2025-11-03 at 18.37.35_749ea508.jpg",
     frameColor: "Red"
   },
   {
-    name: "Team Member 11",
-    role: "Marketing",
-    image: "/assets/photos/teamphotos/IMG_20251123_123325.jpg",
+    name: "Cheyon K",
+    role: "Linguistic Lead",
+    image: "/assets/photos/teamphotos/IMG_20250823_161602.jpg",
+    frameColor: "Green"
+  },
+  {
+    name: "Srinivasan V",
+    role: "Graphic Design Lead",
+    image: "/assets/photos/teamphotos/WhatsApp Image 2025-11-23 at 2.10.09 PM.jpeg",
+    frameColor: "Yellow"
+  },
+  {
+    name: "Tamilarasu",
+    role: "UI/UX Lead",
+    image: "/assets/photos/teamphotos/Polish_20241017_221304347.png",
+    frameColor: "Blue"
+  },
+  {
+    name: "Sanjai Ragul B",
+    role: "Multimedia Lead",
+    image: "/assets/photos/teamphotos/1000019953.jpg",
+    frameColor: "Red"
+  },
+  {
+    name: "Bharanidharan R",
+    role: "Multimedia Lead",
+    image: "/assets/photos/teamphotos/image.png",
     frameColor: "Green"
   },
 ];
