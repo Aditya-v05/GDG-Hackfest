7ed7f297788fa9aec3a0b9c5c975672fd6c3a7b1
pradeeppilot <154545683+Pradeeppilotdev@users.noreply.github.com>
2026-07-04 13:22:10 +0530

    Remove sensitive contact information
    Removed phone numbers and emails for contacts.

--- src/data/contacts.js
+++ src/data/contacts.js
@@ -3,29 +3,21 @@ export const contacts = [
   {
     name: "Chandrapradeep R",
     role: "GDGoC Organizer",
-    phone: "+91 90473 53386",
-    email: "chandrapradeepr@gmail.com",
     bgColor: "bg-pastel-blue"
   },
   {
     name: "Dhanushkumar M",
     role: "Technical Lead",
-    phone: "+91 98422 62221",
-    email: "dhanush.kumar.m@gmail.com",
     bgColor: "bg-pastel-red"
   },
   {
     name: "Tharrunya Vaishnavi P",
     role: "Management Lead",
-    phone: "+91 97512 95071",
-    email: "tharrunyavaisnavi@gmail.com",
     bgColor: "bg-pastel-yellow"
   },
   {
     name: "Aditya V",
     role: "Non-Technical Lead",
-    phone: "+91 63800 07564",
-    email: "adityaspark05@gmail.com",
     bgColor: "bg-pastel-green"
   }
 ];
